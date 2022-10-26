967468037822672c5f8c6241eae305948073628c
Dawid P <dawidpawlata_1994@o2.pl>
2022-10-27 00:27:35 +0200

    kosmetyka

--- app.js
+++ app.js
@@ -16,3 +16,40 @@ function showDivs(n) {
   x[slideIndex-1].style.display = "flex";  
 }
 
+//   $("#footer-arrow").click(function()
+// {
+//     $("#footer-box").toggleClass("footer-box-change")
+    
+// });
+
+function classToggle() {
+  var element = document.getElementById("footer-box");
+  element.classList.toggle("footer-box-change");
+  var arrow = document.getElementById("footer-arrow");
+  arrow.classList.toggle("footer-arrow-change")
+}
+function classToggle2() {
+  var element = document.getElementById("footer-box-2");
+  element.classList.toggle("footer-box-change");
+  var arrow = document.getElementById("footer-arrow-2");
+  arrow.classList.toggle("footer-arrow-change")
+}
+function classToggle3() {
+  var element = document.getElementById("footer-box-3");
+  element.classList.toggle("footer-box-change");
+  var arrow = document.getElementById("footer-arrow-3");
+  arrow.classList.toggle("footer-arrow-change")
+}
+function classToggle4() {
+  var element = document.getElementById("footer-box-4");
+  element.classList.toggle("footer-box-change-buy");
+  var arrow = document.getElementById("footer-arrow-4");
+  arrow.classList.toggle("footer-arrow-change")
+}
+function classToggle5() {
+  var element = document.getElementById("footer-box-5");
+  element.classList.toggle("footer-box-change");
+  var arrow = document.getElementById("footer-arrow-5");
+  arrow.classList.toggle("footer-arrow-change")
+}
+
